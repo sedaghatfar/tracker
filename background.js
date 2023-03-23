1879e80fafc948bd19d363e99cf31109406f67db
Sedaghatfar <Sedaghatfar@gmail.com>
2023-03-23 09:50:12 -0400

    basic timer

--- background.js
+++ background.js
@@ -1,11 +1,17 @@
-function showNotification() {
-  chrome.notifications.create('workTrackerNotification', {
-    type: 'basic',
-    iconUrl: 'icon.png',
-    title: 'Work Tracker',
-    message: 'What are you working on?'
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create("workReminder", {
+    delayInMinutes: 30,
+    periodInMinutes: 30
   });
-}
+});
 
-// Set the interval to show the notification every 30 minutes (1800000 milliseconds)
-setInterval(showNotification, 1800000);
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "workReminder") {
+    chrome.notifications.create("workReminderNotification", {
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Work Reminder",
+      message: "What are you working on?"
+    });
+  }
+});
--- popup.js
+++
@@ -1,5 +0,0 @@
-const logForm = document.getElementById('logForm');
-const exportBtn = document.getElementById('exportBtn');
-const tasks = [];
-
-logForm.addEventListener('submit', (
